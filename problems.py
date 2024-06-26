10df2bff817d49149f1162b497640c38717d001e
Luqman B <luqmanmbutt.dev@gmail.com>
2024-06-26 11:35:47 +0100

    first commit

---
+++ problems.py
@@ -0,0 +1,41 @@
+import random
+# to pass the test function, please return a string of 'string' from then function
+# eg: test() => 'string'
+def test():
+    return "string"
+
+# write a function to remove all empty values (False, [], {}, (), "", None) EXCEPT 0 from an array.
+# It should handle complex data types eg: {}, [] etc.
+# eg: [0, False, "", [], {}, 1, 'Kevin'] => [0, 1, 'Kevin'];
+def remove_blank(list):
+    pass
+
+# write a function to return a random element from an list
+# eg: [1,"elephant", "apple", 67] => "elephant"
+def random_element(list):
+    pass
+
+# write a function that returns the second lowest and second highest number in an list
+# eg: [1,2,3,4,5,6,7,8] => [2,7]
+def second_lowest_second_highest(list):
+    pass
+
+# write a function that will convert a price into coins needed to make up that price
+# the coins available are 1, 2, 5, 10, 20, 50, 100
+# the function should use the smallest number of coins possible
+# eg: coins(1.99) => [100, 50, 20, 20, 5, 2, 2]
+def coins(price):
+    pass
+
+# write a function to merge two lists and remove duplicates
+# eg: mergeUnique([9,8,8,9], [7,8,8,7]) => [9,8,7]
+def merge_unique(list1, list2):
+    pass
+
+# write a function to find the first n fibonacci numbers
+# the fibonacci sequence is a series of numbers, each number is the sum of the last two
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 etc...
+# eg: fibonacci(4) => [0,1,1,2]; fibonacci(8) => [0, 1, 1, 2, 3, 5, 8, 13];
+
+def fibonacci(n):
+    pass
